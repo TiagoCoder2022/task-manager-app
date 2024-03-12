7bfe025c045ab16cc12153c77c274a3ce7d27965
TiagoCoder2022 <tiagonewalves@hotmail.com>
2024-03-12 14:51:53 -0300

    feat: add global providers to get all tasks

--- app/context/global-provider.js
+++ app/context/global-provider.js
@@ -1,18 +1,42 @@
 "use clinet";
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 import themes from "./themes";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 export const GlobalContext = createContext();
 export const GlobalUpdateContext = createContext();
 
 export const GlobalProvider = ({ children }) => {
   const [selectedTheme, setSelectedThem] = useState(0);
+  const [isLoading, setisLoading] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
   const theme = themes[selectedTheme];
+
+  const allTasks = async () => {
+    setisLoading(true);
+    try {
+      const res = await axios.get("/api/tasks");
+      console.log(res.data);
+
+      setTasks(res.data);
+      setisLoading(false);
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
+  useEffect(() => {
+    allTasks();
+  }, []);
 
   return (
     <GlobalContext.Provider
       value={{
         theme,
+        tasks,
       }}
     >
       <GlobalUpdateContext.Provider value={{}}>
