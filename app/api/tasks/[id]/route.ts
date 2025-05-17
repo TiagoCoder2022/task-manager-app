0cc2afdaefd0a825ca1840c5c819dc153862842f
TiagoCoder2022 <tiagonewalves@hotmail.com>
2025-05-17 16:15:27 -0300

    feat: add route to update a task

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -1,6 +1,7 @@
 import prisma from "@/app/utils/connect";
 import { auth } from "@clerk/nextjs";
 import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
 
 export async function DELETE(
   res: Request,
@@ -25,5 +26,50 @@ export async function DELETE(
   } catch (error) {
     console.log("Error Deleting Task: ", error);
     return NextResponse.json({ error: "Error deleting task", status: 500 });
+  }
+}
+
+const taskUpdateSchema = z.object({
+  title: z.string().min(1, "Title is required"),
+  description: z.string().optional(),
+  date: z.string().min(1, "Date is required"),
+  isCompleted: z.boolean().optional(),
+  isImportant: z.boolean().optional(),
+});
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { id } = params;
+
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const body = await req.json();
+    const parsed = taskUpdateSchema.safeParse(body);
+
+    if (!parsed.success) {
+      return NextResponse.json(
+        { error: parsed.error.format() },
+        { status: 400 }
+      );
+    }
+
+    const updatedTask = await prisma.task.update({
+      where: {
+        id,
+      },
+      data: parsed.data,
+    });
+
+    console.log("TASK Updated: ", updatedTask);
+    return NextResponse.json(updatedTask);
+  } catch (error) {
+    console.error("Error Updating Task:", error);
+    return NextResponse.json({ error: "Error updating task" }, { status: 500 });
   }
 }
